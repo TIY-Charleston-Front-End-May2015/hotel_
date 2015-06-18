dc642406e3f9ace42118325640a0eee14c0c5846
Nathan Lodge <nathanlodge@icloud.com>
2015-06-17 22:53:41 -0400

    added prepend functionality

--- main.js
+++ main.js
@@ -15,29 +15,47 @@ page.init();
 
 var page = {
 
-  init: function () {
-  page.initStyling();
-  page.initEvents();
-  },
-  initStyling: function() {
-  },
-  initEvents: function() {
-  page.loadSubreddits();
-  },
-  loadSubreddits: function () {
-    $.ajax({
-      url: baseURL + '.json',
-      method: 'GET',
-      success: function(data) {
-      topThree = data.data.children.slice(0,3);
-      postDetails = _.map(topThree, function(el){
-          return {
-            title: el.data.title,
-            subreddit: el.data.subreddit,
-            url: el.data.url
-        }
-      })
+    init: function () {
+    page.initStyling();
+    page.initEvents();
+    },
+    initStyling: function() {
+    },
+    initEvents: function() {
+    page.loadSubreddits();
+    $('#subredditSearch').on('change', function(e){
+      e.preventDefault();
+      page.addMoreSubreddits();
+    })
+    },
+    loadSubreddits: function () {
+      $.ajax({
+        url: baseURL + '.json',
+        method: 'GET',
+        success: function(data) {
+        topThree = data.data.children.slice(0,3);
+        postDetails = _.map(topThree, function(el){
+            return {
+              title: el.data.title,
+              subreddit: el.data.subreddit,
+              url: el.data.url
+            }
+          })
+        var postTmpl = _.template($('#postTmpl').html());
+
+            _.each(postDetails, function (el) {
+              // console.log(videoTmpl(el));
+              $('.container').prepend(postTmpl(el));
+            });
+            },
+            error: function (err) {
+              console.log("subreddit error:", err);
+            }
+        });
+      },
+    addMoreSubreddits: function () {
+      baseURL = baseURL + 'r/' + $('#subredditSearch').val();
+      page.loadSubreddits();
+      baseURL = 'http://www.reddit.com/';
     }
-  })
-}
 }
